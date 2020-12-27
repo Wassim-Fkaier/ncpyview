24078a1fbf5f523a25ac2caf3ed797a9947dd42b
Wassim Fkaier <wassim.fkaier@outlook.com>
2020-12-27 19:32:53 +0100

    Delete README.md

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 """Setup file"""
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 package_name = "ncpyview"
 version = "1.0.0"
 author = "Wassim Fkaier"
@@ -12,8 +9,8 @@ setuptools.setup(
     version=version,
     author=author,
     author_email="wassfk@outlook.com",
-    description="Interactive web application for netcdf file data visualization",
-    long_description=long_description,
+    description="Interactive web application",
+    long_description="Interactive web application for netcdf file data visualization",
     long_description_content_type="text/markdown",
     # url="https://github.com/pypa/sampleproject",
     packages=setuptools.find_packages(),
